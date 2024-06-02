d9c945e6453a88a92fe13f5a7e9957eb3ed71797
Gadza88 <vasiliew.renat@gmail.com>
2024-06-02 06:38:46 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,77 @@ public class Main {
 
         System.out.println("Hello world!");
 
+        System.out.println("Task 1 ");
+
+        int[] numbers = new int[]{1, 2, 3};
+        double[] fractionalNumbers = {1.57, 7.654, 9.986};
+        int[] myNumbers = {27, 322, 88};
+
+        System.out.println("Task 2 ");
+        for (int i = 0; i < numbers.length; i++) {
+            if (i == numbers.length - 1) {
+                System.out.print(numbers[i]);
+            } else {
+                System.out.print(numbers[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < fractionalNumbers.length; i++) {
+            if (i == fractionalNumbers.length - 1) {
+                System.out.print(fractionalNumbers[i]);
+            } else {
+                System.out.print(fractionalNumbers[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            if (i == myNumbers.length - 1) {
+                System.out.print(myNumbers[i]);
+            } else {
+                System.out.print(myNumbers[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("Task 3 ");
+
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(numbers[i]);
+            } else {
+                System.out.print(numbers[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = fractionalNumbers.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(fractionalNumbers[i]);
+            } else {
+                System.out.print(fractionalNumbers[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = myNumbers.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(myNumbers[i]);
+            } else {
+                System.out.print(myNumbers[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("Task 4");
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 != 0) {
+                numbers[i]++;
+            }
+            System.out.print(numbers[i] + " ");
+        }
 
     }
 }
